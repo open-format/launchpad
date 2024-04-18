8a135a751c1563fb907c794acf3e69b53e5a9e21
Andy <andy@openformat.tech>
2024-04-18 17:39:23 +0100

    Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function Home() {
   return (
-    <div>
+    <div className="max-w-[350px]">
       <Button>Shadcn Button</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
