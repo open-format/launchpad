6c8383bec9221a16f0affa591ad5e4ea1f3571b2
Andy <andy@openformat.tech>
2024-04-22 21:42:42 +0100

    onboarding pages and forms

---
+++ app/onboarding/create-app/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CreateAppForm } from "@/forms/create-app-form";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+export default function CreateAppPage() {
+  const router = useRouter();
+
+  async function onCreateApp(data) {
+    alert(JSON.stringify(data));
+    return router.push("/home/apps");
+  }
+
+  return (
+    <div className="flex flex-col items-center space-y-5 w-full">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Create dApp</CardTitle>
+          <CardDescription>
+            Create a decentralised App.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <CreateAppForm onSubmit={onCreateApp} />
+        </CardContent>
+      </Card>
+      <Link href="/onboarding/create-app">Skip</Link>
+    </div>
+  );
+}
---
+++ app/onboarding/layout.tsx
@@ -0,0 +1,7 @@
+export default function LoginLayout({ children }) {
+  return (
+    <div className="max-w-prose mx-auto h-screen flex items-center justify-center">
+      {children}
+    </div>
+  );
+}
