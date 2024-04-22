257a42317740f651de8ff9e5b9b32eeaa8c58bc1
Andy <andy@openformat.tech>
2024-04-22 21:34:54 +0100

    add custom font and update global styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <div className="max-w-[350px]">
-      <Button>Shadcn Button</Button>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+  redirect("/login");
 }
