3d2975e750edc19d9b6b122294c7eb5790d2bacf
Andy <andy@openformat.tech>
2024-04-23 15:33:49 +0100

    add emailSignup value to loginUserSchema

--- lib/user-schema.ts
+++ lib/user-schema.ts
@@ -1,9 +1,10 @@
-import { object, string, TypeOf } from "zod";
+import { TypeOf, boolean, object, string } from "zod";
 
 export const loginUserSchema = object({
   email: string({ required_error: "Email is required" })
     .min(1, "Email is required")
     .email("Invalid email or password"),
+  emailSignup: boolean(),
 });
 
 export type LoginUserInput = TypeOf<typeof loginUserSchema>;
