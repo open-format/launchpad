6ab92ad45d1c529ccc2eefe606fba91057c6aa07
Andy <andy@openformat.tech>
2024-05-07 14:13:33 +0100

    adds app data to view app page

--- app/home/apps/[id]/loading.tsx
+++ app/home/apps/[id]/loading.tsx
@@ -1,4 +1,3 @@
-import { getApp } from "@/app/_actions";
 import ChainName from "@/components/chain-name";
 import { Badge } from "@/components/ui/badge";
 import {
@@ -7,6 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   Tooltip,
   TooltipContent,
@@ -16,30 +16,10 @@ import {
 import ValueBox from "@/components/value-box";
 import { InfoIcon } from "lucide-react";
 
-async function getData(app: string) {
-  try {
-    const res = await getApp(app);
-    return res;
-  } catch (e: any) {
-    //@TODO Improve error handling
-    console.log(e.message);
-  }
-}
-
-export default async function ViewAppPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const app = await getData(params.id);
-
+export default async function Loading() {
   return (
     <div className="space-y-5">
-      {app && (
-        <h1 className="text-3xl font-bold leading-none tracking-tight">
-          {app.name}
-        </h1>
-      )}
+      <Skeleton className="w-[250px] h-[30px]" />
       <Card>
         <CardHeader>
           <CardTitle>Keys</CardTitle>
@@ -64,26 +44,23 @@ export default async function ViewAppPage({
           </div>
         </CardHeader>
         <CardContent>
-          {app && (
-            <div className="space-y-4">
-              {app.id && (
-                <ValueBox
-                  label="App ID"
-                  description="This the App ID used to interact with your onchain application."
-                  value={app?.id}
-                  copyText="App ID copied to clipboard."
-                />
-              )}
-              {app.xpToken.id && (
-                <ValueBox
-                  label="XP Token Address"
-                  description="This the on-chain token address for the XP token associated with this dApp. This value is required for the SDK only."
-                  value={app?.xpToken.id}
-                  copyText="App ID copied to clipboard."
-                />
-              )}
-            </div>
-          )}
+          <div className="space-y-4">
+            <ValueBox
+              label="App ID"
+              description="This the App ID used to interact with your onchain application."
+              value=""
+              copyText="App ID copied to clipboard."
+              isLoading={true}
+            />
+
+            <ValueBox
+              label="XP Token Address"
+              description="This the on-chain token address for the XP token associated with this dApp. This value is required for the SDK only."
+              value=""
+              isLoading={true}
+              copyText="App ID copied to clipboard."
+            />
+          </div>
         </CardContent>
       </Card>
       {/* <div className="flex justify-between">
