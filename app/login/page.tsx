75e991fc81899c97e8b518bed82bdd6b4dd60817
Andy <andy@openformat.tech>
2024-05-09 16:05:32 +0100

    adds branding and intro to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import getUserSession from "@/lib/getUserSession";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import { LoginForm } from "./login-form";
 
@@ -12,20 +13,25 @@ export default async function LoginPage() {
   }
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="flex items-center justify-center py-12">
-        <div className="mx-auto grid w-[350px] gap-6">
-          <div>
-            <h1 className="text-3xl font-bold">Welcome</h1>
-            <h2 className="text-sm font-medium text-muted-foreground">
-              Sign in to your account
-            </h2>
-          </div>
-          <LoginForm />
-        </div>
+      <div className="fixed flex items-center justify-center lg:justify-start w-full gap-2 font-semibold p-4">
+        <Image
+          className="rounded-md"
+          src="https://avatars.githubusercontent.com/u/121942809?s=200&v=4"
+          height={25}
+          width={25}
+          alt="hi"
+        />
+        <span>OPENFORMAT</span>
       </div>
-      <div className="hidden bg-muted lg:flex h-screen items-end border-l ">
-        <div className="flex items-center space-x-2 m-3 justify-center p-5">
-          <blockquote className="space-y-2 text-right">
+      <div className="hidden bg-muted lg:flex flex-col justify-between h-screen border-l p-5">
+        <div className="space-y-2 my-24">
+          <h1 className="text-5xl max-w-[400px]">
+            Welcome to the Launchpad 🚀
+          </h1>
+          <h3>The no-code blockchain rewards platform.</h3>
+        </div>
+        <div className="flex flex-col items-center space-x-2 m-3 justify-center">
+          <blockquote className="space-y-2 text-left">
             <p className="text-lg">
               "Using OPENFORMAT to power our first Web3 enabled
               project took all of the hassle out of dealing with smart
@@ -35,6 +41,17 @@ export default async function LoginPage() {
               Jack - Developer
             </footer>
           </blockquote>
+        </div>
+      </div>
+      <div className="flex items-center justify-center py-12 text-center">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div>
+            <h1 className="text-3xl font-bold">Welcome</h1>
+            <h2 className="text-sm font-medium text-muted-foreground">
+              Sign in to your account
+            </h2>
+          </div>
+          <LoginForm />
         </div>
       </div>
     </div>
