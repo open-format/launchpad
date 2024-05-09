16d8e00173ea8bf9bf95726f6065a971662e1b5f
Andy <andy@openformat.tech>
2024-05-09 10:35:20 +0100

    adds Google OAuth login functionality

--- app/login/login-form.tsx
+++ app/login/login-form.tsx
@@ -3,8 +3,10 @@
 import { Button } from "@/components/ui/button";
 import useSupabaseClient from "@/lib/supabase/client";
 import { GitHubLogoIcon, ReloadIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import googleIcon from "../images/icons/google.svg";
 
 export const LoginForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -20,16 +22,42 @@ export const LoginForm = () => {
     });
   }
 
+  const loginWithGoogle = () => {
+    setIsLoading(true);
+    supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+  };
+
   return (
     <div>
-      <Button disabled={isLoading} onClick={loginWithGitHub}>
-        {isLoading ? (
-          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <GitHubLogoIcon className="mr-2 h-4 w-4" />
-        )}
-        Continue With Github
-      </Button>
+      <div className="space-y-2">
+        <Button disabled={isLoading} onClick={loginWithGitHub}>
+          {isLoading ? (
+            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <GitHubLogoIcon className="mr-2 h-4 w-4" />
+          )}
+          Continue With Github
+        </Button>
+        <Button disabled={isLoading} onClick={loginWithGoogle}>
+          {isLoading ? (
+            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            <Image
+              src={googleIcon}
+              alt="google icon"
+              width={16}
+              height={16}
+              className="mr-2 h-4 w-4"
+            />
+          )}
+          Continue With Google
+        </Button>
+      </div>
       <div className="my-8 self-center text-sm text-center">
         <span className="text-foreground-light">
           Don't have an account?
