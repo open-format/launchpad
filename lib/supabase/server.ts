9d89ebccc32422c4814f32df89fd024c76893028
Andy <andy@openformat.tech>
2024-05-09 13:44:39 +0100

    updates supabase server client to enable admin rights via service key

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,12 +6,16 @@ import {
 } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export default async function createSupabaseServerClient() {
+export default async function createSupabaseServerClient(
+  admin?: boolean
+) {
   const cookieStore = cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    admin
+      ? process.env.SUPABASE_SERVICE_ROLE_KEY!
+      : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
