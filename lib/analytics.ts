f18af815b67ef36b78c25835e1bbf88a9a4fe9e2
Andy <andy@openformat.tech>
2024-05-10 14:05:14 +0100

    add analytics event handler

---
+++ lib/analytics.ts
@@ -0,0 +1,53 @@
+import { headers } from "next/headers";
+
+type AnalyticsEvent = {
+  event_name: string;
+  event_meta?: {
+    [key: string]: string;
+  };
+};
+
+function getHeaders(): {
+  ip: string;
+  userAgent: string;
+  protocol: string;
+  domain: string;
+} {
+  let ip = "0.0.0.0";
+  const forwardedFor = headers().get("x-forwarded-for");
+
+  if (forwardedFor) {
+    ip = forwardedFor.split(",")[0] ?? ip;
+  } else {
+    ip = headers().get("x-real-ip") ?? ip;
+  }
+
+  const userAgent = headers().get("user-agent") ?? "";
+  const domain = headers().get("x-forwarded-host") || "";
+  const protocol = headers().get("x-forwarded-proto") || "";
+
+  return { ip, userAgent, protocol, domain };
+}
+export async function trackEvent(event: AnalyticsEvent) {
+  const { ip, userAgent, protocol, domain } = getHeaders();
+
+  try {
+    const res = await fetch("https://api.pirsch.io/api/v1/event", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.PIRSCH_KEY}`,
+      },
+      body: JSON.stringify({
+        event_name: event.event_name,
+        url: `${protocol}://${domain}`,
+        ip: ip,
+        accept_language: "en-US",
+        user_agent: userAgent,
+      }),
+    });
+    return { event, status: res.status };
+  } catch (e) {
+    return e;
+  }
+}
