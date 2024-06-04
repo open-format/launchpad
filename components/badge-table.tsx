de115fb485fa9a12b59ab78b85e9769dca12acef
Andy <andy@openformat.tech>
2024-06-04 07:59:37 +0100

    fixes error message on badge creation

--- components/badge-table.tsx
+++ components/badge-table.tsx
@@ -23,7 +23,7 @@ export default function BadgeTable({
   trackEvent,
 }: BadgeTableProps) {
   if (!badges || !badges.length) {
-    return <EmptyState />;
+    return <EmptyState trackEvent={trackEvent} />;
   }
   return (
     <div className="space-y-4">
@@ -46,7 +46,11 @@ export default function BadgeTable({
   );
 }
 
-function EmptyState() {
+function EmptyState({
+  trackEvent,
+}: {
+  trackEvent: TrackEventFunction;
+}) {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="w-[350px] text-center flex items-center justify-center flex-col">
@@ -63,8 +67,7 @@ function EmptyState() {
           badge to reward users for their achievements and boost
           engagement.
         </p>
-        {/* @ts-ignore */}
-        <CreateBadgeDialog />
+        <CreateBadgeDialog trackEvent={trackEvent} />
       </div>
     </div>
   );
